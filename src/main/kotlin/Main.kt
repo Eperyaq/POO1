bd1ea0afcdadf430eaa50573118de810473655f5
eperyaq205 <eperyaq205@g.educaand.es>
2024-01-19 21:13:54 +0100

    corregido en clase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.lang.IllegalArgumentException
+
 /*
 1. Crear una clase Rectángulo, con atributos base y altura. La clase debe disponer del constructor y los métodos para calcular el area y el perimetro. Los atributos no se podrán modificar, aunque si consultar. Por último, tendrán que ser mayor que 0.
 
@@ -6,23 +8,26 @@
 3. En el programa principal, crear varios rectángulos. Mostarlos y mostrar por pantalla sus áreas y perímetros.
 **/
 
-class Rectangulo(base: Double, altura: Double, color: String){
+class Rectangulo(val base: Double, val altura: Double) {
 
-    constructor(base:Double = 1.0, altura:Double = 1.0) : this(base, altura, color = "Red") { // No entiendo porque no me deja...
-        var base: Double = this
+    var area: Double = 0.0
+        get() = calcularArea()
+        private set
+
+    var perimetro: Double = 0.0
+        get() = calcularPerímetro()
+        private set
+
+    init {
+        require(base > 0.0 && altura > 0.0) { "La base y la altura deben  ser mayor que 0." }
     }
 
-    fun calcularArea(base: Double, altura: Double): Double{
-        var area = (base * altura)
+    constructor(base:Int, altura:Int) : this(base.toDouble(), altura.toDouble())
 
-        return area
-    }
+    private fun calcularArea() = (base * altura)
 
-    fun calcularPerímetro(base: Double, altura:Double): Double{
-        var perimetro = 2 * (base + altura)
+    fun calcularPerímetro() = 2 * (base + altura)
 
-        return perimetro
-    }
 
     override fun toString(): String { //???
         return super.toString()
@@ -32,13 +37,22 @@ class Rectangulo(base: Double, altura: Double, color: String){
 
 
 fun main() {
-    var miRectangulo = Rectangulo(20.0, 10.0)
-    println("El área del rectangulo es ${miRectangulo.calcularArea(20.0, 10.0)}") //No hay alguna manera de meterle los datos que ya tiene la variable??
-    println("El perímetro de mi rectangulo es ${miRectangulo.calcularPerímetro(20.0 ,10.0)}")
+    try {
+        val miRectangulo = Rectangulo(7.0, 0.0)
+    }
+    catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 
-    var tuRectangulo = Rectangulo()
-    println("El area de TU rectangulo es ${tuRectangulo.calcularArea()}")//No deberian pasarse los valores por defecto?
-    println("El area de TU rectangulo es ${tuRectangulo.calcularPerímetro()}")
+    val miRectangulo = Rectangulo(7.0, 5.0)
+
+
+    println("El área del rectangulo es ${miRectangulo.area}")
+    println("El perímetro de mi rectangulo es ${miRectangulo.perimetro}")
+
+    var tuRectangulo = Rectangulo(10, 7)
+    println("El area de TU rectangulo es ${tuRectangulo.area}")
+    println("El area de TU rectangulo es ${tuRectangulo.perimetro}")
 
 }
 
