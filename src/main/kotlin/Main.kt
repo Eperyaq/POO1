1a54a5874ea5ee3b671382a2419e49f0e951fc84
Eliaser <eperyaq@g.educaand.es>
2024-01-27 11:15:23 +0100

    ejercico terminado bien

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,3 @@
-import java.lang.IllegalArgumentException
-
 /*
 1. Crear una clase Rectángulo, con atributos base y altura. La clase debe disponer del constructor y los métodos para calcular el area y el perimetro. Los atributos no se podrán modificar, aunque si consultar. Por último, tendrán que ser mayor que 0.
 
@@ -8,51 +6,27 @@ import java.lang.IllegalArgumentException
 3. En el programa principal, crear varios rectángulos. Mostarlos y mostrar por pantalla sus áreas y perímetros.
 **/
 
-class Rectangulo(val base: Double, val altura: Double) {
+class Rectangulo(base: Double, altura: Double) {
 
-    var area: Double = 0.0
-        get() = calcularArea()
-        private set
+    fun calcularArea(base: Double, altura: Double): Double {
+        var area = (base * altura)
 
-    var perimetro: Double = 0.0
-        get() = calcularPerímetro()
-        private set
-
-    init {
-        require(base > 0.0 && altura > 0.0) { "La base y la altura deben  ser mayor que 0." }
+        return area
     }
 
-    constructor(base:Int, altura:Int) : this(base.toDouble(), altura.toDouble())
+    fun calcularPerímetro(base: Double, altura: Double): Double {
+        var perimetro = 2 * (base + altura)
 
-    private fun calcularArea() = (base * altura)
-
-    fun calcularPerímetro() = 2 * (base + altura)
-
-
-    override fun toString(): String { //???
-        return super.toString()
+        return perimetro
     }
 }
-
-
 
 fun main() {
-    try {
-        val miRectangulo = Rectangulo(7.0, 0.0)
-    }
-    catch (e: IllegalArgumentException) {
-        println(e.message)
-    }
+    var miRectangulo = Rectangulo(20.0, 10.0)
+    println("El área del rectangulo es ${miRectangulo.calcularArea(20.0, 10.0)}")
+    println("El perímetro de mi rectangulo es ${miRectangulo.calcularPerímetro(20.0, 10.0)}")
 
-    val miRectangulo = Rectangulo(7.0, 5.0)
-
-
-    println("El área del rectangulo es ${miRectangulo.area}")
-    println("El perímetro de mi rectangulo es ${miRectangulo.perimetro}")
-
-    var tuRectangulo = Rectangulo(10, 7)
-    println("El area de TU rectangulo es ${tuRectangulo.area}")
-    println("El area de TU rectangulo es ${tuRectangulo.perimetro}")
-
+    var tuRectangulo = Rectangulo(12.5, 22.0)
+    println("El area de TU rectangulo es ${tuRectangulo.calcularArea(12.5, 22.0)}")
+    println("El perimetro de TU rectangulo es ${tuRectangulo.calcularPerímetro(12.5, 22.0)}")
 }
-
